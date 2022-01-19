dc8abfbce9df025a9c674de537355e74a2738639
SilviaNurMahmudah <silvia.mahmudah@gmail.com>
2022-01-19 08:07:04 +0700

    revisi controller

--- app/Varietas.php
+++ app/Varietas.php
@@ -3,13 +3,25 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Bentuk;
 use App\Ukuran;
+use App\WarnaDaging;
+use App\WarnaKulit;
 
 class Varietas extends Model
 {
     protected $table = "varietas";
 
+    public function bentuk() {
+        return $this->belongsTo(Bentuk::class);
+    }
     public function ukuran() {
         return $this->belongsTo(Ukuran::class);
+    }
+    public function wdaging() {
+        return $this->belongsTo(WarnaDaging::class);
+    }
+    public function wkulit() {
+        return $this->belongsTo(WarnaKulit::class);
     }
 }
